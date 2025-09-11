7ca5322ec439e9883aa28cef49d343731b567f68
nnguyen05 <2255146@cegep-heritage.qc.ca>
2025-09-11 11:32:53 -0400

    rudimentary comma parsing working...

--- csv.c
+++ csv.c
@@ -1,14 +1,33 @@
 #include "csv.h"
+#include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #define BUFFER_SIZE 256
 
+#define RESIZE(cursor, start, size, type) \
+if(cursor >= (start + size)){ \
+	ptrdiff_t diff = cursor - start; \
+	size += BUFFER_SIZE; \
+	start = realloc(start, size*sizeof(type)); \
+	cursor = start + diff; \
+} \
+
 typedef struct {
 	char** lines;
-	size_t count;
+	size_t cLine;
 } lines_t;
+
+typedef struct {
+	char** columns;
+	size_t cColumn;
+} row_t;
+
+typedef struct {
+	row_t* rows;
+	size_t cRow;
+} csv_t;
 
 size_t getlines(const char* path, lines_t* lines){
 	FILE* fFile = fopen(path, "rb");
@@ -16,7 +35,7 @@ size_t getlines(const char* path, lines_t* lines){
 	if(fFile == NULL) return -1;
 	
 	size_t cLines = BUFFER_SIZE;
-	lines->count = 0;
+	lines->cLine = 0;
 	lines->lines = (char**)calloc(cLines, sizeof(char*));
 	char** ppLines = lines->lines;
 	
@@ -30,26 +49,17 @@ size_t getlines(const char* path, lines_t* lines){
 		// ignore marks of the beast
 		if(iChar == '\r') continue;
 
-		if(pchLine >= (szLine + cLine)){
-			ptrdiff_t bLine = pchLine - szLine;
-			cLine += BUFFER_SIZE;
-			szLine = realloc(szLine, cLine*sizeof(char));
-			pchLine = szLine + bLine;
-		}
+		RESIZE(pchLine, szLine, cLine, char);
 		*pchLine = iChar;
 
 		if(iChar == '\n'){
 			szLine = realloc(szLine, cbBuffer + 1);
 			szLine[cbBuffer] = 0;
 			
-			if(ppLines >= (lines->lines + cLines)){
-				ptrdiff_t bLines = ppLines - lines->lines;
-				cLines += BUFFER_SIZE;
-				lines->lines = realloc(lines->lines, cLines*sizeof(char*));
-				ppLines = lines->lines + bLines;
-			}
+			RESIZE(ppLines, lines->lines, cLines, char*);
 			*(ppLines++) = szLine;
-			lines->count++;
+
+			lines->cLine++;
 
 			cLine = BUFFER_SIZE;
 			szLine = (char*)calloc(cLine, sizeof(char*));
@@ -60,13 +70,14 @@ size_t getlines(const char* path, lines_t* lines){
 			cbBuffer++;
 		}
 	}
-	lines->lines = realloc(lines->lines, lines->count*sizeof(char*));
+	lines->lines = realloc(lines->lines, lines->cLine*sizeof(char*));
+	fclose(fFile);
 
-	return lines->count;
+	return lines->cLine;
 }
 
 void freelines(lines_t* lines){
-	for(int i = 0; i < (int)lines->count; i++){
+	for(size_t i = 0; i < lines->cLine; i++){
 		free(lines->lines[i]);
 		lines->lines[i] = NULL;
 	}
@@ -74,12 +85,65 @@ void freelines(lines_t* lines){
 	lines->lines = NULL;
 }
 
+size_t linestocsv(lines_t* lines, csv_t* csv){
+	csv->cRow = lines->cLine;
+	csv->rows = (row_t*)calloc(csv->cRow, sizeof(row_t));
+	row_t* pRow = csv->rows;
+	for(size_t i=0; i<lines->cLine; i++){
+		char* line = lines->lines[i];
+		char* start = line;
+		size_t length = strlen(line) + 1;
+
+		size_t cBufColumn = BUFFER_SIZE;
+		pRow->cColumn = 0;
+		pRow->columns = (char**)calloc(cBufColumn, sizeof(char*));
+
+		char** pColumn = pRow->columns;
+
+		for(int i=0; i<(int)length; i++){
+			switch(line[i]){
+				case ',':
+				case '\0': {
+					line[i] = '\0';
+					RESIZE(pColumn, pRow->columns, cBufColumn, char*);
+					
+					*(pColumn++) = start;
+					pRow->cColumn++;
+					
+					start = line + i + 1;
+					break;
+				}
+			}
+		}
+		pRow->columns = realloc(pRow->columns, pRow->cColumn*sizeof(char*));
+
+		pRow++;
+	}
+	return csv->cRow;
+}
+
+void freecsv(csv_t* csv){
+	for(size_t i=0; i<csv->cRow; i++){
+		free(csv->rows[i].columns);
+		csv->rows[i].columns = NULL;
+	}
+	free(csv->rows);
+	csv->rows = NULL;
+}
+
 int do_csv(const char* path){
 	lines_t lines = {0};
+	csv_t csv = {0};
 	getlines(path, &lines);
-	
-	for(int i = 0; i < (int)lines.count; i++){
-		printf("%s\n", lines.lines[i]);
+
+	linestocsv(&lines, &csv);
+
+	for(size_t i=0; i<csv.cRow; i++){
+		row_t row = csv.rows[i];
+		for(size_t j=0; j < row.cColumn; j++){
+			printf("(%s), ", row.columns[j]);
+		}
+		printf("\n");
 	}
 
 	freelines(&lines);
